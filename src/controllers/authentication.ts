e21a44694f65a74924b4263a534416b3e51006c9
Baran Emre Turkmen <baranemreturkmen@gmail.com>
2025-06-07 18:34:17 +0300

    Implement user login functionality in authentication controller; update router to include login endpoint

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -2,7 +2,41 @@ import express from "express";
 import { getUserByEmail, createUser } from "../db/users";
 import { random, authentication } from "../helpers";
 
-export const register = async (req: express.Request, res: express.Response): Promise<void> => {
+export const login = async (req: express.Request, res: express.Response) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            res.sendStatus(400);
+            return;
+        }
+
+        const user = await getUserByEmail(email).select("+authentication.salt +authentication.password");
+        if (!user) {
+            res.sendStatus(400);
+            return;
+        }
+
+        const expectedHash = authentication(user.authentication.salt, password);
+        if (user.authentication.password !== expectedHash) {
+            res.sendStatus(403);
+            return;
+        }
+
+        const salt = random();
+        user.authentication.sessionToken = authentication(salt, user._id.toString());
+
+        await user.save();//Burada save yapmak ne kadar mantıklı?
+
+        res.cookie("NODE-TS-AUTH", user.authentication.sessionToken, { domain: "localhost", path: "/" });
+        return res.status(200).json(user).end();
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+};
+
+export const register = async (req: express.Request, res: express.Response) => {
     try {
         const { email, password, username } = req.body;
         if (!email || !password || !username) {
@@ -24,9 +58,9 @@ export const register = async (req: express.Request, res: express.Response): Pro
                 password: authentication(salt, password)
             }
         });
-        res.status(200).json(user);
+        return res.status(200).json(user).end();//burada response'da tüm user'a ait bilgileri göndermemek lazım.(Örneğin: password, salt)
     } catch (error) {
         console.log(error);
-        res.sendStatus(400);
+        return res.sendStatus(400);
     }
 };
