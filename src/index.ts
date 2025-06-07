5ca16d855dce5ace97be590e36adda4f420276bf
Baran Emre Turkmen <baranemreturkmen@gmail.com>
2025-06-07 14:35:06 +0300

    Update package dependencies and implement basic Express server with MongoDB connection, UserModel Created.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
-console.log("Hello Node!");
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.use(cors({
+    credentials: true,
+}));
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+    console.log("Server is running on port http://localhost:8080");
+});
+
+const MONGO_URL = 'mongodb+srv://baranemre1997:yFdvkL007QnLrgsy@cluster0.gtjj2b8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on("error", (error: Error) => {
+    console.log("❌ MongoDB Connection Error:", error);
+});
+
+mongoose.connection.on("connected", () => {
+    console.log("✅ Successfully connected to MongoDB.");
+});
