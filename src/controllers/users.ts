f8d3d50487a09e19b8357f9efd38107de809ff24
Baran Emre Turkmen <baranemreturkmen@gmail.com>
2025-06-07 22:23:44 +0300

    Enhance user management functionality by adding updateUser controller and isOwner middleware; update user router to support user updates

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,4 +1,4 @@
-import { deleteUserById, getUsers } from "../db/users";
+import { deleteUserById, getUserById, getUsers, updateUserById } from "../db/users";
 import express from "express";
 
 export const getAllUsers = async (req: express.Request, res: express.Response) => {
@@ -16,6 +16,24 @@ export const deleteUser = async (req: express.Request, res: express.Response) =>
         const { id } = req.params;
         const deletedUser = await deleteUserById(id);
         return res.json(deletedUser);
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+};
+
+export const updateUser = async (req: express.Request, res: express.Response) => {
+    try {
+        const { id } = req.params;
+        const { username } = req.body;
+
+        const user = await getUserById(id);
+        if (!user) {
+            return res.sendStatus(400);
+        }
+
+        const updatedUser = await updateUserById(id, username);
+        return res.json(updatedUser).end();
     } catch (error) {
         console.log(error);
         return res.sendStatus(400);
--- src/router/users.ts
+++ src/router/users.ts
@@ -1,10 +1,14 @@
 import express from "express";
-import { deleteUser, getAllUsers } from "../controllers/users";
-import { isAuthenticated } from "../middlewares";
+import { deleteUser, getAllUsers, updateUser } from "../controllers/users";
+import { isAuthenticated, isOwner } from "../middlewares";
 
 export default (router: express.Router) => {
     router.get("/users", isAuthenticated as unknown as express.RequestHandler
         , getAllUsers as unknown as express.RequestHandler);
-    router.delete("/users/:id", isAuthenticated as unknown as express.RequestHandler
+    router.delete("/users/:id", isAuthenticated as unknown as express.RequestHandler //make sure isAuthenticated first before isOwner
+        ,isOwner as unknown as express.RequestHandler 
         , deleteUser as unknown as express.RequestHandler);
+    router.patch("/users/:id", isAuthenticated as unknown as express.RequestHandler
+        ,isOwner as unknown as express.RequestHandler 
+        , updateUser as unknown as express.RequestHandler);
 };
